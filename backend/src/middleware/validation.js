807847374e537291903e27f1642d2746e6fa0fc4
katiromero2107-svg <katiromero2107@gmail.com>
2026-06-02 21:20:57 -0300

    Adicionar middleware de validação

---
+++ backend/src/middleware/validation.js
@@ -0,0 +1,14 @@
+const { validationResult } = require('express-validator');
+
+const handleValidationErrors = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ 
+      error: 'Erro de validação',
+      details: errors.array() 
+    });
+  }
+  next();
+};
+
+module.exports = handleValidationErrors;
